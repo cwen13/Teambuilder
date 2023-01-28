1fdbd46b8a3f3150ab2f0b89ab802c35542e5bff
cwen13 <cwenrich13@gmail.com>
2023-01-28 17:21:26 -0500

    workingo n looping or recursion

--- src/script.js
+++ src/script.js
@@ -34,13 +34,12 @@ let interns = [];
 let engineers = [];
 let managers = [];
 let questions = [];
+let roles = [];
 
-function gatherTheTeam() {
-  let building = true;
-  while (building){
-  inquirer.prompt(mainQuestion)
+function addAnotherMember() {
+  let response = inquirer.prompt(mainQuestion)
       .then((response) => {
-	if (response.addMember) {
+	if(response.addMember) {
 	  switch (response.role){
 	  case "Intern":
 	    questions = employeeQuestions.concat(internQuestion);
@@ -48,47 +47,95 @@ function gatherTheTeam() {
 	  case "Engineer":
 	    questions = employeeQuestions.concat(engineerQuestion);	    
 	    break;
-          case "Manager":
+	  case "Manager":
 	    questions = employeeQuestions.concat(managerQuestion);	    
 	    break;
 	  }
 	} else {
-	  building = false;
+	  questions = null;
 	}
-	inquirer.prompt(questions)
-	  .then((personelInfo) => {
-	    switch(response.role) {
-	    case "Intern":
-	      interns.push(Intern(personelInfo.name,
+	return questions;
+      });
+}
+
+function addTheMember(role, personelInfo) {
+    switch(role) {
+    case "Intern":
+      interns.push(Intern(personelInfo.name,
+			  personelInfo.id,
+			  personelInfo.email,
+			  personelInfo.school));
+      break;
+    case "Engineer":
+      engineers.push(Intern(personelInfo.name,
+			    personelInfo.id,
+			    personelInfo.email,
+				personelInfo.github));
+      break;
+    case "Manager":
+      managers.push(Intern(personelInfo.name,
+			   personelInfo.id,
+				   personelInfo.email,
+			   personelInfo.officeNumber));
+      
+      break;
+    }
+  return 0;
+}
+
+function gatherTheTeam() {
+  let role = "";
+  inquirer.prompt(mainQuestion)
+    .then((response) => {
+      role = response.role;
+      if(response.addMember){
+	switch (role) {
+	case "Intern":
+	  questions = employeeQuestions.concat(internQuestion);
+	  break;
+	case "Engineer":
+	  questions = employeeQuestions.concat(engineerQuestion);	    
+	  break;
+	case "Manager":
+	  questions = employeeQuestions.concat(managerQuestion);	    
+	  break;
+	}
+      }
+      inquirer.prompt(questions)
+	.then((personelInfo) => {
+	  switch(role) {
+	  case "Intern":
+	    interns.push(Intern(personelInfo.name,
+				personelInfo.id,
+				personelInfo.email,
+				personelInfo.school));
+	    break;
+	  case "Engineer":
+	    engineers.push(Intern(personelInfo.name,
 				  personelInfo.id,
 				  personelInfo.email,
-				  personelInfo.school));
-	      break;
-	    case "Engineer":
-	      engineers.push(Intern(personelInfo.name,
-				    personelInfo.id,
-				    personelInfo.email,
-				    personelInfo.github));
-	      break;
-	    case "Manager":
-	      managers.push(Intern(personelInfo.name,
-				   personelInfo.id,
-				   personelInfo.email,
-				   personelInfo.officeNumber));
-	      
-	      break;
-	      }
-	    })
-      });
-  }
+				  personelInfo.github));
+	    break;
+	  case "Manager":
+	    managers.push(Intern(personelInfo.name,
+				 personelInfo.id,
+				 personelInfo.email,
+				 personelInfo.officeNumber));
+	    
+	    break;
+	  }
+	  gatherTheTeam();
+	});
+    });
 
   html += managers.forEach(getCard)
     + engineers.forEach(getCard)
     + interns.forEach(getCard)
     + footer;
-
+  
   return html;
 }
+
 
 function writeHTML(HTML) {
   fs.writeFile("../dist/index.html", HTML, (err) =>
