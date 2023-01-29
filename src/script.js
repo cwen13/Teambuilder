7e4142be2404843782994b52af0bdd8cb4150e05
cwen13 <cwenrich13@gmail.com>
2023-01-28 20:54:58 -0500

    Got promises to resolve and prompts moving

--- src/script.js
+++ src/script.js
@@ -55,12 +55,15 @@ const internQuestion = [
 //===================================================
 
 
-let mainQuestions = [
+let addQuestion = [
   {
     type: "confirm",
     message: "Do you want to add another memeber to the team?",
     name: "addMember"
-  },
+  }
+];
+
+let roleQuestion = [
   {
     type:"list",
     message: "What role will they fill?",
@@ -69,11 +72,45 @@ let mainQuestions = [
   }
 ];
 
-const { bootstrap,
-	fontAwe,
-	header,
-	bodyTop,
-	footer } = require("../lib/html");
+// const { bootstrap,
+// 	fontAwe,
+// 	header,
+// 	bodyTop,
+// 	footer } = require("../lib/html");
+//==================================================
+const bootstrap = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">`;
+
+const fontAwe = `<link rel="stylesheet" href="https://kit.fontawesome.com/a284b5a489.css" crossorigin="anonymous">`;
+
+const header = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  <meta name="Team Builder" content="A CLI that creates a team for you witha few questions"/>
+  ${bootstrap}
+  ${fontAwe}
+  <title>The Team's Team</title>
+</head>`;
+
+const bodyTop = `<body class="min-vh-100 d-flex align-items-center bg-dark">
+    <section class="w-100 d-flex flex-row flex-wrap justify-content-around">`;
+
+
+const footer = `    </section>
+  </div>
+</body>
+  <footer>
+    Made by Teambuild
+    Developed by Cody Wenrich
+    Powered by <i class="fa-brands fa-github"></i><a href="https://github.com/cwen13> Github</a>
+  </footer>
+</html>`;
+
+//==================================================
+
 let html = header + bodyTop;
 
 // Variables 
@@ -138,11 +175,13 @@ function addTheMember(role, personelInfo) {
 }
 
 async function gatherTheTeam() {
-  let {addMember, role} = await inquirer.prompt(mainQuestions);
-  console.log(role);
-  console.log(typeof employeeQuestions);
-  if(addMember) {
-    switch (role){
+//  let role = "";
+  while (true) {
+    response = await inquirer.prompt(addQuestion);   
+    if (!response.addMember) break;
+ 
+    response = await inquirer.prompt(roleQuestion);
+    switch (response.role){
     case "Intern":
       questions = employeeQuestions.concat(internQuestion);
       break;      
@@ -153,46 +192,43 @@ async function gatherTheTeam() {
       questions = employeeQuestions.concat(managerQuestion);	    
       break;
     }
-  } else {
-    questions = null;
+    
+    if (questions) {
+      let person = await inquirer.prompt(questions)
+	  .then((info) => {
+	    switch(response.role) {
+	    case "Intern":
+	      interns.push(new intern(info.name,
+				      info.id,
+				      info.email,
+				      info.school));
+	      break;
+	    case "Engineer":
+	      engineers.push(new engineer(info.name,
+					  info.id,
+					  info.email,
+					  info.github));
+	      break;
+	    case "Manager":
+	      managers.push(new manager(info.name,
+					info.id,
+					info.email,
+					info.officeNumber));
+	      
+	      break;
+	    }
+	  });
+    }
   }
 
-  if (questions) {
-    let person = await inquirer.prompt(questions)
-      .then((info) => {
-	if (addMember) {
-	  switch(role) {
-	  case "Intern":
-	    interns.push(new intern(info.name,
-				info.id,
-				info.email,
-				info.school));
-	    break;
-	  case "Engineer":
-	    engineers.push(new engineer(info.name,
-				  info.id,
-				  info.email,
-				  info.github));
-	    break;
-	  case "Manager":
-	    managers.push(new manager(info.name,
-				 info.id,
-				 info.email,
-				 info.officeNumber));
-	    
-	    break;
-	  }
-	  //	  gatherTheTeam();
-	}
-      });
-  }
-    
-    html += managers.forEach(manager.prototype.getCard)
-    + engineers.forEach(engineer.prototype.getCard)
-    + interns.forEach(intern.prototype.getCard)
+
+  html += managers.map((manager) => manager.getCard())
+    + engineers.map((engineer) => engineer.getCard())
+    + interns.map((intern) => intern.getCard())
     + footer;
   
-  return html;
+  console.log(html);
+    return html;
 }
 
 
